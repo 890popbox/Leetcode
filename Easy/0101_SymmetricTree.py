3320334a99f815abc1d9e18dcbb1e17cc8cea578
Chris <890popbox@users.noreply.github.com>
2023-10-15 21:17:17 -0700

    Update Trees

--- Easy/0101_SymmetricTree.py
+++ Easy/0101_SymmetricTree.py
@@ -1,22 +1,26 @@
-def isSameTree(p, q):
+# This solution is the SameTree inverted
+def isSymmetric(root):
+    # Base case check
+    if root is None:
+        return True
     # Let's try this iteratively, we will use a stack to achieve this
-    s = [p, q]
+    s = [root.left, root.right]
     while s:
-        # Pop off the first tree, then the second
-        p, q = s.pop(), s.pop()
+        # Pop off the tree
+        l, r = s.pop(), s.pop()
 
         # If both exist, add it to the stack
-        if p and q:
+        if l and r:
             # If the values don't match, tree if not the same
-            if p.val != q.val:
+            if l.val != r.val:
                 return False
             # Push the correct sides in properly if they exist or not
-            s.append(p.left)
-            s.append(q.left)
-            s.append(p.right)
-            s.append(q.right)
+            s.append(l.left)
+            s.append(r.right)
+            s.append(l.right)
+            s.append(r.left)
         # If one exists but the other doesn't, tree is not the same
-        elif p or q:
+        elif l or r:
             return False
         # By checking these the last thing to consider is if both are None, which would be True, keep scanning Tree
     # The while loop handles 3 cases and if it never returns False, both Trees must be the same

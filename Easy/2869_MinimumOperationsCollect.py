8457d93e1a4b5ea853acefceaa6527f2d1162aff
Chris <890popbox@users.noreply.github.com>
2023-11-21 00:22:50 -0800

    Minimum Operations Improved

--- Easy/2869_MinimumOperationsCollect.py
+++ Easy/2869_MinimumOperationsCollect.py
@@ -1,14 +1,15 @@
 def minOperations(nums, k):
-    # Create an array to fill with numbers
-    output, array = 0, []
-    # Run this loop until we fill the array enough
-    while len(array) < k:
-        # Pop the least element from the array
-        val = nums.pop()
-        # If the value is less than or equal to k we can add it to our filler array
-        if val <= k and val not in array:
-            array.append(val)
-        output += 1
-    return output
-
-# Going to update this to a more efficient solution
+    # Create a variable to count
+    count = 0
+    # Run a for loop backwards to look at each element in the array of nums
+    for i in range(len(nums) - 1, -1, -1):
+        val = abs(nums[i]) - 1
+        # Check if the value of the index we are at is less than k or equal to it
+        # Also check if the offset has been marked or not, if it has then this number has been used before
+        if val < k and nums[val] > 0:
+            count += 1
+            nums[val] *= -1
+            if count == k:
+                return len(nums) - i
+    # The count will be found by the time we go through N numbers
+    # O(N) Time and O(1) Space

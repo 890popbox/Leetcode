ad16d5daa0ecd334aa766581c37f7d8d63fbe4f3
Chris <890popbox@users.noreply.github.com>
2023-12-07 00:56:21 -0800

    Update

--- Easy/1991_FindMidIndex.py
+++ Easy/1991_FindMidIndex.py
@@ -3,7 +3,6 @@ def findMiddleIndex(self, nums):
     left, right = 0, sum(nums)
     # Scan through the whole array
     for i in range(0, len(nums)):
-        print(left, right, i)
         if left == right - nums[i]:
             return i
         left += nums[i]

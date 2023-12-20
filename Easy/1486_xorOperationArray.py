d6d3a8c19c9b3e5136281fde1c41101d9aa2a7ae
Chris <890popbox@users.noreply.github.com>
2023-12-19 23:56:33 -0800

    XOR Operation in an Array

--- Easy/1486_xorOperationArray.py
+++ Easy/1486_xorOperationArray.py
@@ -1,8 +1,8 @@
-    def xorOperation(self, n, start):
-        output = 0
-        for i in range(start,start+(2*n),2):
-            output = output ^ i
-        return output
+def xorOperation(self, n, start):
+    output = 0
+    for i in range(start,start+(2*n),2):
+        output = output ^ i
+    return output
 # Create a val to return, instead of storing an array of nums where nums[i] = start + 2 * i
 # Run a loop that deals with each of these numbers, this allows you to deal with each value in place without storing them
 # Use the logical XOR operator on each number and return the output

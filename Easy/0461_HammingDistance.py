f1f43806f8e6d0f2b82d8fb545200e995290f3a9
Chris <890popbox@users.noreply.github.com>
2023-12-22 04:26:07 -0800

    Hamming Distance

--- Easy/0461_HammingDistance.py
+++ Easy/0461_HammingDistance.py
@@ -1,9 +1,9 @@
-    def hammingDistance(self, x, y):
-        c = 0
-        while x or y:
-            if x%2!=y%2:
-                c+=1
-            x/=2
-            y/=2
-        return c
+def hammingDistance(self, x, y):
+    c = 0
+    while x or y:
+        if x%2!=y%2:
+            c+=1
+        x/=2
+        y/=2
+    return c
 # Hold the count and while x or y still exist, count if their remainders are different

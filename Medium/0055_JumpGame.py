8ca6e058d88ff809055e5b42f0257bfc71d08040
Chris <890popbox@users.noreply.github.com>
2024-01-03 19:10:57 -0800

    Jump Game Optimized Part 2

--- Medium/0055_JumpGame.py
+++ Medium/0055_JumpGame.py
@@ -1,3 +1,20 @@
+# Scan from the start to end with possibly to reach end quicker than end to start, O(N)
+def canJump(self, nums):
+    if len(nums) == 1: return True
+    jump, maxjump = 0, nums[0]
+    index, last_index = 0, len(nums) - 1
+    # index is able to reach maxJump or lower with current information given
+    while index <= maxjump:
+        # If the max we can jump ever reachs or passes the last_index this is True
+        if maxjump >= last_index: return True
+        # The farthest place we can get to jumping from current index, update maxJump if its greater than current
+        jump = index + nums[index]
+        if jump > maxjump: maxjump = jump
+        index += 1
+    return False
+
+
+# Alternative option from end to start, O(N)
 def canJump(self, nums):
     last_index = len(nums) - 1
     # Scan backwards from the second to last index

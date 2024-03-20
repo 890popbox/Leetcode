89d0b4961ac061a3ba6ac0c39af03d00920c388d
Chris <890popbox@users.noreply.github.com>
2024-03-19 18:38:13 -0700

    Generate Parentheses

---
+++ Medium/0022_GenerateParentheses.py
@@ -0,0 +1,31 @@
+    def generateParenthesis(self, n):
+        result = []
+        left = right = 0
+        q = [(left, right, '')]
+        while q:
+            left, right, s = q.pop()
+            if len(s) == 2 * n:
+                result.append(s)
+            if left < n:
+                q.append((left + 1, right, s + '('))
+            if right < left:
+                q.append((left, right + 1, s + ')'))
+        return result
+
+# Recursive method
+'''
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        res = []
+        dfs(0, 0, '')
+        return res
+'''
